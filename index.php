ebf842dda74fba9612e0acfe3683d51913df51a8
EstiakAhmedPial <69714173+EstiakAhmedPial@users.noreply.github.com>
2020-08-15 22:30:17 +0600

    Add files for the first time
    Minimum design complete

---
+++ index.php
@@ -0,0 +1,339 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!--    Slick CSS-->
+    <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css"/>
+
+
+    <title>Food Park</title>
+
+<!-- Style -->
+<style type="text/css">
+
+
+
+
+
+
+
+
+
+</style>
+<!-- Style -->
+
+
+
+  </head>
+  <body >
+  <div class="container">
+      
+<!--  NAV Menu Bar -->
+
+   <div class="row container-fluid">
+      <div class="col">
+           <nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+              <a class="navbar-brand" href="#">
+                  <h2>
+                      <em>Food Park</em>
+                  </h2>
+              </a>
+              
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              
+                  <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                      <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                      </li>
+                    </ul>
+                    
+                    <span class="navbar-text " style="">
+                      Order Online Your Favourite Food
+                    </span>
+                  </div>
+            </nav>
+      </div>
+   </div>
+
+<!--  NAV Menu Bar -->
+
+   
+<!--   Header Ends     -->
+   
+ 
+   
+<!--     body   -->
+
+
+<div class="row">
+  <br>
+  <!-- Empty Space -->
+</div>
+
+<!-- Items Slider -->
+  <div class="row">
+    <div class="col">
+      
+        <div class="row">
+          
+    <!--      ITEMS        -->
+<!--
+            <div class="col-md-2">
+              <div class="card border-warning text-center" style="width: 95%;  ">
+                <img src="img/biriany-1.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                <div class="card-body">
+                  <h6 class="card-title">Chicken Biriany</h6>
+                  <span class="badge badge-danger">Price: 100 TK</span>
+
+                  <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-warning">Order it</a>
+                </div>
+              </div>
+            </div>
+-->
+    <!--      ITEMS        -->
+
+
+
+          </div>
+        </div>
+
+    </div>
+ 
+<!-- Items Slider -->
+
+<!--   Slider     -->
+          <div class="aslider-class multiple-items">
+            <div>your content</div>
+            <div>your content</div>
+            <div>your content</div>
+          </div>
+        
+        <!--   Slider     -->
+
+<!--Main row start-->
+<div class="row">
+    <div class="col-md-9 col-sm-9 border border-warning">
+        
+    </div>    
+    
+    <div class="col-md-3 col-sm-3 border border-warning ">
+        
+    </div>     
+                
+</div>
+<!--Main row End-->
+<!-- Flicker Slide -->
+
+      <div class="yourElement border border-warning "><!-- Main container -->
+
+        <div class="fcontainer row"><!-- Flickable elements container (required) -->
+          <div class="fcontainerInner "><!-- (required) -->
+            
+            <div id="1" class="item col-md-4">
+              <!--      ITEMS        -->
+                
+                  <div class="card border-warning text-center" style="width: 95%;  ">
+                    <img src="img/biriany-1.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Chicken Biriany</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+                
+              <!--      ITEMS        -->
+            </div><!-- must have `item' for class name -->
+            
+            <div id="2" class="item col-md-4">
+              <!--      ITEMS        -->
+               
+                  <div class="card border-warning text-center" style="width: 95%; ">
+                    <img src="img/pizza-3.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Item pizza-3</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+            
+              <!--      ITEMS        -->
+            </div>
+            
+            <div id="3" class="item col-md-4">
+              <!--      ITEMS        -->
+                
+                  <div class="card border-warning text-center" style="width: 95%;   ;">
+                    <img src="img/biriany-Mutton.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Mutton biriany</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+                
+            <!--      ITEMS        -->
+           </div> 
+          
+          <div id="4" class="item col-md-4">
+              <!--      ITEMS        -->
+                
+                  <div class="card border-warning text-center" style="width: 95%;  ">
+                    <img src="img/avocado-bun-burgers-11a.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Avocado Bun Burgers</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+                
+              <!--      ITEMS        -->
+        </div><!-- must have `item' for class name -->
+            
+            <div id="5" class="item col-md-4">
+              <!--      ITEMS        -->
+               
+                  <div class="card border-warning text-center" style="width: 95%; ">
+                    <img src="img/spicy-chicken-burgers-3.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Spicy Chicken Burgers</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+            
+              <!--      ITEMS        -->
+            </div>
+            <div id="6" class="item col-md-4">
+              <!--      ITEMS        -->
+                
+                  <div class="card border-warning text-center" style="width: 95%;   ;">
+                    <img src="img/dd7822b4b7802ced3651044d13b51a36.jpg" class="card-img-top" alt="..." style="height: 180px;">
+                    <div class="card-body">
+                      <h6 class="card-title">Mutton burger</h6>
+                      <span class="badge badge-danger">Price: 100 TK</span>
+                      <p class="card-text font-weight-lighter">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-warning">Order it</a>
+                    </div>
+                  </div>
+                </div>
+    <!--      ITEMS        -->
+            </div>  
+        </div>
+    
+
+        <div class="row">
+          <div class="col-md-10 ">
+            <div class="nav "><!-- Tab, indicator or something like that (optional) -->
+              <ul class="list list-inline text-center">
+                <li class="list-inline-item"><a class="text-decoration-none" href="#1" >&#9679;</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none" href="#2">&#9679;</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none" href="#3">&#9679;</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none" href="#4">&#9679;</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none" href="#5">&#9679;</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none" href="#6">&#9679;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="arrows"><!-- Next and prev buttons (optional) -->
+                  <span class="prev float-left badge badge-danger">Previous</span><!-- must have `prev' for className -->
+                  <span class="next float-right badge badge-danger">Next </span><!-- must have `next' for className -->
+                </div>
+            </div>
+        </div><br>
+        
+      </div>
+    
+<!-- Flicker Slide-->
+
+
+
+
+<!--     body   -->
+
+    
+
+
+
+    
+    
+<!--    Footer Starts   -->
+   <div class="row justify-content-center bg-warning " style="margin-top: 10px;">
+    <div class="col-md-4">
+            
+                
+            <p class="h6 " style="color:#fcfcf9; ">2020 &copy;Copyright all rights reserved. <a href="#">ID-123</a> </p>
+                
+            
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    
+    
+</div>
+     <!-- Optional JavaScript -->
+<!--  Slick Slider -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+   <script type="text/javascript" src="js/slick.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+          $('.aslider-class').slick({
+            setting-name: setting-value
+          });
+        });
+        
+        $('.multiple-items').slick({
+          infinite: true,
+          slidesToShow: 3,
+          slidesToScroll: 3
+        });
+    </script>
+
+<!--  Slick Slider -->
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+      <script src="./js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="./js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+    
+    
+
+  </body>
+</html>
